01727f58a5b1b2621b31f0f981d0d0c9fa55af6d
2mxas <t.hernandez11@uniandes.edu.co>
2026-05-12 08:36:05 -0500

    Key Guard de comentario agregada

--- src/posts/posts.controller.ts
+++ src/posts/posts.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, UseGuards } from '@nestjs/common';
 import { PostsService } from './posts.service';
 import { CreateCommentDto } from './dto/create-comment.dto';
+import { ApiKeyGuard } from 'src/auth/api-key.guard';
 
 @Controller('posts')
 export class PostsController {
   constructor(private readonly postsService: PostsService) { }
 
   @Post(':id/comments')
+  @UseGuards(ApiKeyGuard)
   createComment(@Param('id') id: string, @Body() createCommentDto: CreateCommentDto) {
     return this.postsService.createComment(id, createCommentDto);
   }
